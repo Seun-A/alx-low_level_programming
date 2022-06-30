721d757614e2cc32436cf1a4c0ed47c70214c819
Seun-A <73300414+Seun-A@users.noreply.github.com>
2022-06-30 14:27:24 +0100

    Betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
